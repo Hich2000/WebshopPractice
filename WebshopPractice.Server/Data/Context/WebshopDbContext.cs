6b19fa87317268a5c11566239e42cfc5fa1bc0c9
Hich2000 <33690542+Hich2000@users.noreply.github.com>
2026-03-21 11:14:57 +0100

    Add project files.

---
+++ WebshopPractice.Server/Data/Context/WebshopDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebshopPractice.Server.Data.Models;
+
+namespace WebshopPractice.Server.Data.Context;
+
+public class WebshopDbContext : DbContext
+{
+    public WebshopDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
---
+++ WebshopPractice.Server/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using WebshopPractice.Server.Data.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configure SQLite DbContext
+builder.Services.AddDbContext<WebshopDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.MapStaticAssets();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapFallbackToFile("/index.html");
+
+app.Run();

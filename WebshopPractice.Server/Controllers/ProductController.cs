6250b09e7b47d8b696c1b4b4cb2a6ea7efbda791
Hich2000 <33690542+Hich2000@users.noreply.github.com>
2026-03-22 11:40:24 +0100

    simple product controller, only gets some random products for now.

---
+++ WebshopPractice.Server/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using WebshopPractice.Server.Data.Models;
+
+namespace WebshopPractice.Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProductController : ControllerBase
+{
+
+    private static readonly string[] productNames =
+    [
+        "productA", "productB", "productC", "productD", "productE"
+    ];
+
+    [HttpGet(Name = "GetProduct")]
+    public IEnumerable<Product>? Get()
+    {
+        return Enumerable.Range(0, 4).Select(index => new Product { 
+            Id = index,
+            Name = productNames[index],
+            Price = 1.50M
+        }).ToArray();
+    }
+}

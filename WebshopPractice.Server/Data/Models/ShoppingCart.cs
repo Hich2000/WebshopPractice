2fd00bda09a3755be500950f373b6a65137806a0
Hich2000 <33690542+Hich2000@users.noreply.github.com>
2026-03-22 11:01:42 +0100

    Added simple user and shoppingcart models

---
+++ WebshopPractice.Server/Data/Models/ShoppingCart.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebshopPractice.Server.Data.Models;
+
+public class ShoppingCart
+{
+    [Key]
+    public int Id { get; set; }
+    public IEnumerable<Product>? Products { get; set; }
+
+    //user coupling
+    public int UserId { get; set; }
+    public required User User { get; set; }
+}

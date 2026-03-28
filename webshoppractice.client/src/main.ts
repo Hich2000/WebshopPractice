6cc48eea2fd103f1ab022a188c403c2858b8dc0c
Hich2000 <33690542+Hich2000@users.noreply.github.com>
2026-03-28 13:12:05 +0100

    added primevue component library

--- webshoppractice.client/src/main.ts
+++ webshoppractice.client/src/main.ts
@@ -1,6 +1,19 @@
 import './assets/main.css'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+    .use(PrimeVue, {
+        theme: {
+            preset: Aura
+        }
+    });
+
+// components
+import Button from 'primevue/button'
+app.component('Button', Button)
+
+app.mount('#app');

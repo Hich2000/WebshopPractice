eaeb385f44bd9fd4d0ac4bdbc5fa6602a9459920
Hich2000 <33690542+Hich2000@users.noreply.github.com>
2026-04-04 20:21:34 +0200

    added most of the necessary endpoints for user management

--- WebshopPractice.Server/Controllers/ShopUserController.cs
+++ WebshopPractice.Server/Controllers/ShopUserController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebshopPractice.Server.Data.Context;
@@ -8,6 +8,7 @@ using WebshopPractice.Server.Helpers;
 namespace WebshopPractice.Server.Controllers;
 
 [ApiController]
+[Authorize]
 [Route("[controller]")]
 public class ShopUserController(
     WebshopDbContext db
@@ -18,36 +19,99 @@ public class ShopUserController(
     private readonly WebshopDbContext _db = db;
 
     [HttpGet]
-    [Route("Table")]
-    public async Task<PaginatedTable<ShopUserDTO>> GetTable(int PageNumber = 1, int PageSize = _smallestPageLength)
+    [Route("Paged")]
+    public async Task<PaginatedTable<ShopUserDTO>> GetPaged(int pageNumber = 1, int pageSize = _smallestPageLength)
     {
-        if (!_allowedPageLength.Contains(PageSize)) PageSize = _smallestPageLength;
-        if (PageNumber < 1) PageNumber = 1;
+        if (!_allowedPageLength.Contains(pageSize)) pageSize = _smallestPageLength;
+        if (pageNumber < 1) pageNumber = 1;
 
-        var UserSlice = await _db.ShopUsers
-                     .OrderBy(u => u.Id)
-                     .Skip((PageNumber - 1) * PageSize)
-                     .Take(PageSize)
-                     .ToListAsync();
+        List<ShopUserDTO> userSlice = await _db.ShopUsers
+            .AsNoTracking()
+            .OrderBy(u => u.Id)
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .Select(u => new ShopUserDTO
+            {
+                UserId = u.Id,
+                Email = u.Email,
+                Name = u.Name,
+                UserLevel = u.UserLevel
+            })
+            .ToListAsync();
 
-        int TotalRecordCount = await _db.ShopUsers.CountAsync();
-        int PageCount = TotalRecordCount / PageSize;
+        int totalRecordCount = await _db.ShopUsers.CountAsync();
+        int pageCount = (int)Math.Ceiling((double)(totalRecordCount / pageSize));
 
         return new PaginatedTable<ShopUserDTO>
         {
-            PageNumber = PageNumber,
-            PageCount = PageCount,
-            PageSize = PageSize,
-            TotalRecordCount = TotalRecordCount,
-            Body = UserSlice.Select(u =>
-                    new ShopUserDTO
-                    {
-                        UserId = u.Id,
-                        Email = u.Email,
-                        Name = u.Name,
-                        UserLevel = u.UserLevel
-                    }
-                ).ToList()
+            PageNumber = pageNumber,
+            PageCount = pageCount,
+            PageSize = pageSize,
+            TotalRecordCount = totalRecordCount,
+            Body = userSlice
         };
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<ShopUserDTO>> Get(string id)
+    {
+        var user = await _db.ShopUsers
+        .Where(u => u.Id == id)
+        .Select(u => new ShopUserDTO
+        {
+            UserId = u.Id,
+            Email = u.Email,
+            Name = u.Name,
+            UserLevel = u.UserLevel
+        })
+        .FirstOrDefaultAsync();
+
+        if (user == null) return NotFound();
+        return Ok(user);
+    }
+
+    [HttpPatch("{id}")]
+    public async Task<IActionResult> Patch(string id, [FromBody] ShopUserDTO updatedUser)
+    {
+        if (!ModelState.IsValid) return BadRequest(ModelState);
+        if (id != updatedUser.UserId) return BadRequest();
+
+        try
+        {
+            await _db.ShopUsers
+            .Where(u => u.Id == updatedUser.UserId)
+            .ExecuteUpdateAsync(u => u
+                .SetProperty(p => p.Email, p => updatedUser.Email)
+                .SetProperty(p => p.Name, p => updatedUser.Name)
+            );
+
+            return Ok(updatedUser);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        try
+        {
+            var rowsAffected = await _db.ShopUsers
+                .Where(u => u.Id == id)
+                .ExecuteDeleteAsync();
+
+            if (rowsAffected == 0)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+        catch (Exception)
+        {
+            return NotFound();
+        }
     }
 }
